eea5aac39d1ac00eeac10d8e85fe3e9ac60b47be
Francisco Ramirez <ramiirez100@gmail.com>
2018-10-27 13:49:20 +0200

    Cambios Layout

--- cliente/Tema-3/Calculadora/codigo.js
+++ cliente/Tema-3/Calculadora/codigo.js
@@ -1,79 +1,98 @@
+/**
+ * 
+ * Calculadora sin funcionalidad
+ * 
+ * @author Francisco Ramírez Ruiz
+ */
 {
-  let botones = [
-    "CE",
-    "DEL",
-    "%",
-    "+",
-    "7",
-    "8",
-    "9",
-    "-",
-    "4",
-    "5",
-    "6",
-    "x",
-    "1",
-    "2",
-    "3",
-    "/",
-    "0",
-    "+/-",
-    ",",
-    "="
-  ];
-  let contador = 0;
 
   function init() {
-    calculadora();
+    crearCalculadora();
+
   }
 
-  let calculadora = function() {
-    let header = document.createElement("header");
+  /**
+   * Creo el layout de la calculadora
+   */
 
-    let h1 = document.createElement("h1");
-    h1.textContent = "Calculadora - Francisco Ramirez";
-    h1.style.color = "red";
-    h1.style.textAlign = "center";
-    let main = document.createElement("main");
-    main.style.textAlign = "center";
-    let container = document.createElement("container");
+  let crearCalculadora = function () {
+    //Contador para recorrer el array
+    let contador = 0;
 
-    let div = document.createElement("div");
-
-    let input = document.createElement("input");
-    input.type = "text";
-    input.style.width = '20%';
+    //Creo un array de Ids para obtener un id distinto a cada botón
+    let arrayIds = ["btnCE", "btnC", "btnPorc", "btnSuma", "btn7", "btn8", "btn9", "btnResta", "btn4", "btn5", "btn6", "btnMult", "btn1", "btn2", "btn3", "btnDiv", "btn0", "btnCambSig", "btnComa", "btnIgual"]
+    // Array de botones para sus respectivos textos
+    let botones = ["CE", "DEL", "%", "+", "7", "8", "9", "-", "4", "5", "6", "x", "1", "2", "3", "/", "0", "+/-", ",", "="];
 
     /**
-     * Método con los appendChild
+     * Div calculadora donde agrupo todo
      */
-    aniadir(header, h1, main, container, div, input);
+    let contenido = document.createElement("div");
+    contenido.className = "calculadora";
+    /**
+     * Creo un h1, con mi Nombre
+     */
+    let h1 = document.createElement("h1");
+    h1.textContent = "Calculadora - Francisco";
 
+    //Creo un elemento div con un input
+    let elemento = document.createElement("div");
+    let input = document.createElement("input");
+
+    //Lo declaro de tipo text
+    input.type = "text";
+    //ClassName para su estilo
+    input.className = "texto";
+
+
+    //Le coloco un id texto.
+    input.id = "texto";
+
+    //Su valor por defecto será 0
+    input.value = 0;
+
+    elemento.appendChild(input);
+    contenido.appendChild(h1);
+    contenido.appendChild(elemento);
+
+    /**
+     * Bucle anidado
+     * 5 Filas de Div
+     * 4 Columnas de botones
+     */
     for (let i = 0; i < 5; i++) {
-      let containerButton = document.createElement("div");
-
+      elemento = document.createElement("div");
       for (let j = 0; j < 4; j++) {
-        let button = document.createElement("button");
-        button.type = "button";
-        button.style.width = '5%';
-        button.style.height = '90%';
-        button.style.fontSize = '20px';
+        //Creo un input de tipo botón.
+        input = document.createElement("input");
+        input.type = "button";
+        /**
+         * Le coloco el valor del array botones (texto)
+         * y su Id correspondiente ()
+         */
+        input.value = botones[contador];
+        //Coloco un className a todos para su estilo.
+        input.className = "button";
+        input.id = arrayIds[contador];
 
-        button.textContent = botones[contador++];
+        contador++;
 
-        containerButton.appendChild(button);
+        elemento.appendChild(input);
       }
-      container.appendChild(containerButton);
+      contenido.appendChild(elemento);
     }
-  };
-  window.addEventListener("load", init);
-}
 
-function aniadir(header, h1, main, container, div, input) {
-  header.appendChild(h1);
-  main.appendChild(container);
-  container.appendChild(div);
-  div.appendChild(input);
-  document.body.appendChild(header);
-  document.body.appendChild(main);
+    imprimirBotones = function () {
+
+    }
+
+    arrayIds.forEach(function () {
+
+
+    });
+    document.body.appendChild(contenido);
+
+  }
+
+  window.addEventListener("load", init);
 }
