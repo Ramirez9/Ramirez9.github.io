214b93b55c85589fe029768c81993657e5162346
Francisco Ramirez <ramiirez100@gmail.com>
2018-11-12 21:27:17 +0100

    Subida de conecta 4

---
+++ android/Conecta 4/src/main/java/com/example/francisco/conecta4/InitialActivity.java
@@ -0,0 +1,63 @@
+package com.example.francisco.conecta4;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+public class InitialActivity extends AppCompatActivity {
+
+    private Button btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setTitle("");
+        setContentView(R.layout.activity_initial);
+
+        btn = (Button) findViewById(R.id.btnInicio);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainActivity);
+            }
+        });
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.menuAbout){
+
+            Intent acercaDe = new Intent(getApplicationContext(), About.class);
+            startActivity(acercaDe);
+
+            return true;
+        }else if (id == R.id.menuSendMessage) {
+
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "Estoy jugando al conecta 4!!!");
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
+
+        }else if (id == R.id.menuSettings){
+            startActivity(new Intent(this, Settings.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
