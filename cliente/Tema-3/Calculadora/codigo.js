d129c997cb6e4227d62acae415adc9fa91593fa7
Francisco Ramirez <ramiirez100@gmail.com>
2018-11-20 18:12:01 +0100

    Calculadora Objeto Literal

--- cliente/Tema-3/Calculadora/codigo.js
+++ cliente/Tema-3/Calculadora/codigo.js
@@ -4,35 +4,15 @@
  * Controlando divisiones infinitas
  * Decimales
  * Y acumulador para concatenar operaciones
+ * 
+ * Modificamos el código para trabajarlo con un objeto literal
  *
- * @version 2.0
+ * @version 3.0
  * @author Francisco Ramírez Ruiz
  */
 
 {
-  //Creo un array de Ids para obtener un id distinto a cada botón
-  let botonesId = [
-    "btnCE",
-    "btnDEL",
-    "btnPorcentaje",
-    "btnSuma",
-    "btn7",
-    "btn8",
-    "btn9",
-    "btnResta",
-    "btn4",
-    "btn5",
-    "btn6",
-    "btnMult",
-    "btn1",
-    "btn2",
-    "btn3",
-    "btnDiv",
-    "btn0",
-    "btnSigno",
-    "btnPunto",
-    "btnIgual"
-  ];
+
 
   // Array de botones para sus respectivos textos
   let botones = [
@@ -58,91 +38,106 @@
     "="
   ];
 
-  //Declaro y asigno las variables para la version 2.0
-  let numero;
   let texto;
-  let acumulado = "";
-  let operacionBandera = false;
   let entrada;
 
   function init() {
     //Creo la calculadora
-    crearCalculadora();
+    calculadora.crearCalculadora();
     //Obtengo la clases de texto "texto"
     texto = document.getElementsByClassName("texto")[0];
-    entrada = acciones(entrada, botonesId);
+    entrada = acciones(entrada, calculadora.botonesId);
   }
 
   /**
    * Creo el layout de la calculadora
    */
 
-  let crearCalculadora = function() {
-    //Contador para recorrer el array
-    let contador = 0;
+  let calculadora = {
 
-    /**
-     * Div calculadora donde agrupo todo
-     */
-    let contenido = document.createElement("div");
-    contenido.className = "calculadora";
+    numero: 0,
+    acumulado: '',
+    operacionBandera: false,
 
-    /*
-     * Creo un h1, con mi Nombre
-     */
-    let h1 = document.createElement("h1");
-    h1.textContent = "Calculadora - Francisco";
+    //Creo un array de Ids para obtener un id distinto a cada botón
+    botonesId: [
+      "btnCE", "btnDEL", "btnPorcentaje", "btnSuma",
+      "btn7", "btn8", "btn9", "btnResta",
+      "btn4", "btn5", "btn6", "btnMult",
+      "btn1", "btn2", "btn3", "btnDiv",
+      "btn0", "btnSigno", "btnPunto", "btnIgual"
+    ],
 
-    //Creo un elemento div con un input
-    let elemento = document.createElement("div");
-    let input = document.createElement("input");
+    crearCalculadora: function () {
 
-    //Lo declaro de tipo text
-    input.type = "text";
-    //ClassName para su estilo
-    input.className = "texto";
 
-    //Le coloco un id texto.
-    input.id = "texto";
+      //Contador para recorrer el array
+      let contador = 0;
 
-    //Su valor por defecto será 0
-    input.value = 0;
+      /**
+       * Div calculadora donde agrupo todo
+       */
+      let contenido = document.createElement("div");
+      contenido.className = "calculadora";
 
-    //Añado las etiquetas a sus padres
-    elemento.appendChild(input);
-    contenido.appendChild(h1);
-    contenido.appendChild(elemento);
+      /*
+       * Creo un h1, con mi Nombre
+       */
+      let h1 = document.createElement("h1");
+      h1.textContent = "Calculadora - Francisco";
 
-    /**
-     * Bucle anidado
-     * 5 Filas de Div
-     * 4 Columnas de botones
-     */
-    for (let i = 0; i < 5; i++) {
-      elemento = document.createElement("div");
-      console.log(elemento);
-      for (let j = 0; j < 4; j++) {
-        //Creo un input de tipo botón.
-        input = document.createElement("input");
-        input.type = "button";
-        /**
-         * Le coloco el valor del array botones (texto)
-         * y su Id correspondiente ()
-         */
-        input.value = botones[contador];
-        //Coloco un className a todos para su estilo.
-        input.className = "button";
-        input.id = botonesId[contador];
+      //Creo un elemento div con un input
+      let elemento = document.createElement("div");
+      let input = document.createElement("input");
 
-        contador++;
+      //Lo declaro de tipo text
+      input.type = "text";
+      //ClassName para su estilo
+      input.className = "texto";
 
-        elemento.appendChild(input);
-      }
+      //Le coloco un id texto.
+      input.id = "texto";
+
+      //Su valor por defecto será 0
+      input.value = 0;
+
+      //Añado las etiquetas a sus padres
+      elemento.appendChild(input);
+      contenido.appendChild(h1);
       contenido.appendChild(elemento);
+
+      /**
+       * Bucle anidado
+       * 5 Filas de Div
+       * 4 Columnas de botones
+       */
+      for (let i = 0; i < 5; i++) {
+        elemento = document.createElement("div");
+        console.log(elemento);
+        for (let j = 0; j < 4; j++) {
+          //Creo un input de tipo botón.
+          input = document.createElement("input");
+          input.type = "button";
+          /**
+           * Le coloco el valor del array botones (texto)
+           * y su Id correspondiente ()
+           */
+          input.value = botones[contador];
+          //Coloco un className a todos para su estilo.
+          input.className = "button";
+          input.id = calculadora.botonesId[contador];
+
+          contador++;
+
+          elemento.appendChild(input);
+        }
+        contenido.appendChild(elemento);
+      }
+
+      document.body.appendChild(contenido);
     }
 
-    document.body.appendChild(contenido);
-  };
+  } //Calculadora
 
   /**
    * Funcionalidad de la calculadora.
@@ -151,29 +146,29 @@
     //Parseo los números enteros y distintos de NaN
     if (!isNaN(parseInt(this.value))) {
       //Controlo que la operacionBandera no sea true
-      if (operacionBandera) 
-      texto.value = 0;
+      if (calculadora.operacionBandera)
+        texto.value = 0;
       //Si el texto es 0, valor 0
       //Compruebo la coma, para no machacar el 0
-      if (texto.value == 0  && !texto.value.includes(".")) 
-      texto.value = parseFloat(this.value);
+      if (texto.value == 0 && !texto.value.includes("."))
+        texto.value = parseFloat(this.value);
       //Concadeno los números
       else texto.value += parseFloat(this.value);
 
       //La colo de nuevo a false, por si estaba true.
-      operacionBandera = false;
+      calculadora.operacionBandera = false;
     } else {
       //Sino pulso ni números ni esos botones, llamo a operacionBandera()
       if (
-        acumulado != "" &&
+        calculadora.acumulado != "" &&
         this.value != "CE" &&
         this.value != "+/-" &&
         this.value != "," &&
         this.value != "DEL" &&
         this.value != "%"
       )
-      //Metodo operacionBandera
-      operacion();
+        //Metodo operacionBandera
+        operacion();
 
       comprobacion(this.value);
 
@@ -184,7 +179,7 @@
         this.value != "CE" &&
         this.value != "DEL"
       )
-        operacionBandera = true;
+        calculadora.operacionBandera = true;
     }
   }
 
@@ -194,54 +189,54 @@
    * @param {*} value 
    */
   function comprobacion(value) {
-    numero = acumuladoBasico(value, numero, texto);
+    calculadora.numero = acumuladoBasico(value, calculadora.numero, texto);
     switch (value) {
       //acumulado +
       case "+":
-        acumulado = "+";
+        calculadora.acumulado = "+";
         break;
-      //acumulado -
+        //acumulado -
       case "-":
-        acumulado = "-";
+        calculadora.acumulado = "-";
         break;
-      //acumulado x
+        //acumulado x
       case "x":
-        acumulado = "x";
+        calculadora.acumulado = "x";
         break;
-      //acumulado /
+        //acumulado /
       case "/":
-        acumulado = "/";
+        calculadora.acumulado = "/";
         break;
-      //acumulado +/-
+        //acumulado +/-
       case "+/-":
         //multiplico el texto por -1
         texto.value = parseFloat(texto.value) * -1;
         break;
-      //acumulado ,
+        //acumulado ,
       case ",":
-        if (!operacionBandera) {
+        if (!calculadora.operacionBandera) {
           //Sino incluye punto se le añade al texto.
           if (!texto.value.includes(".")) texto.value += ".";
         }
         break;
-      //acumulado DEL
+        //acumulado DEL
       case "DEL":
-      texto.value = texto.value.substring(0, texto.value.length - 1);
+        texto.value = texto.value.substring(0, texto.value.length - 1);
         if (texto.value.length < 1 || texto.value === "-" || texto.value === "-0.")
           texto.value = 0;
         break;
-      //acumulado CE
+        //acumulado CE
       case "CE":
         //texto vale 0
         texto.value = 0;
-        acumulado = "";
+        calculadora.acumulado = "";
         break;
-      //acumulado %
+        //acumulado %
       case "%":
         //valor del texto entre 100
         texto.value = parseFloat(texto.value) / 100;
         break;
-      //case =
+        //case =
       case "=":
         //llamo al metodo operacionBandera
         operacion();
@@ -255,32 +250,32 @@
    * Haho una operación un otra
    */
   function operacion() {
-    if (acumulado != "") {
-      switch (acumulado) {
+    if (calculadora.acumulado != "") {
+      switch (calculadora.acumulado) {
         //Case sumar
         case "+":
-          texto.value = parseFloat(texto.value) + numero;
+          texto.value = parseFloat(texto.value) + calculadora.numero;
           break;
-        //Case restar
+          //Case restar
         case "-":
-          texto.value = numero - parseFloat(texto.value);
+          texto.value = calculadora.numero - parseFloat(texto.value);
           break;
-        //Case multiplicar  
+          //Case multiplicar  
         case "x":
-          texto.value = parseFloat(texto.value) * numero;
+          texto.value = parseFloat(texto.value) * calculadora.numero;
           break;
-        //Case dividir  
+          //Case dividir  
         case "/":
-        //Compruebo que no se divida entre 0
-          if(texto.value == 0)
-            acumulado == 0
+          //Compruebo que no se divida entre 0
+          if (texto.value == 0)
+            calculadora.acumulado == 0
           else
-          texto.value = numero / parseFloat(texto.value);
+            texto.value = calculadora.numero / parseFloat(texto.value);
           break;
       }
     }
     //Asigno valor vacío para que no vuelva a entrar en operacionBandera
-    acumulado = "";
+    calculadora.acumulado = '';
   }
 
   /**
@@ -313,7 +308,3 @@
 
   window.addEventListener("load", init);
 }
-
-
-
-
