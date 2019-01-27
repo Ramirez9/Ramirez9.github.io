432d93d3955376969b7e10a1bbdb80342163fef3
Francisco Ramirez <ramiirez100@gmail.com>
2019-01-27 12:46:51 +0100

    Try y Uso de map Closes #18 Closes#17
    Hago uso del disparado creado anteriormente "seleccionado" el cual se encarga de validar todo los Blur  también elimino el uso del map que hacía en la validación de la reserva para hacerlo con un array el cual me permite ir almacenando los índices.
    Código de aportación.
    let inputErroneos = [];
    inputErroneos.push(elementoErroneo);//???
    
    ...
    if (inputErroneos.length!=0){
    inputErroneos[0].focus();
    return;
    }

--- cliente/Tema-5/ReentregaExamenDiciembre2018/js/main.js
+++ cliente/Tema-5/ReentregaExamenDiciembre2018/js/main.js
@@ -8,13 +8,10 @@
 {
 
     // Declaro las variables necesarias
-    // let nombre;
-    // let correo;
-    // let fecha;
-    // let hora;
-    // let noches;
-    // let personas;
     let inputs;
+    let xSpan;
+    let spans;
+    let enviar;
 
     // Variables checks
     let edad1;
@@ -24,20 +21,6 @@
     let almuerzo;
     let cena;
 
-    //Span de errores de los datos
-    // let xNombre;
-    // let xCorreo;
-    // let xFecha;
-    // let xHora;
-    // let xNoches;
-    // let xPersonas;
-    let xSpan;
-    let spans;
-
-    //Enviar
-    let enviar;
-
-    let map = new Map();
 
     function init() {
 
@@ -54,18 +37,11 @@
 
         //Id de los spans
         spans = Array.from(document.querySelectorAll("body form span"));
-        xSpan = document.getElementById('xSpan');
+        xSpan = document.getElementById('xSpan'); //Muestro error de fecha no calculable.
 
         //Id de boton enviar
         enviar = document.getElementById('enviar');
 
-        //Eventos de Blur en las cajas.
-        // nombre.addEventListener('blur', validarNombre);
-        // correo.addEventListener('blur', validarCorreo);
-        // fecha.addEventListener('blur', validarFecha);
-        // hora.addEventListener('blur', validarHora);
-        // noches.addEventListener('blur', validarNoches);
-        // personas.addEventListener('blur', validarPersonas);
         validarBlur(); //Válido los inputs con blur de la colecction inputs.
 
         //Evento click del botón
@@ -76,26 +52,51 @@
     //Objeto literal con los patrones
     let patron = {
 
-        correo: [/^[\w]+@[\w]+.[\w]+$/,
-            "El correo es inválido, hola@hola.com"
+        //Expresión reuglar nombre y apellido
+        nombre: [/^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\']+[\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])+[\s]?([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\'])?$/,
+            "El nombre es erróneo. Ejemplo: Francisco Ramírez"
+        ],
 
+         //Expresión reuglar correo electrónico
+        correo: [/^[\w]+@[\w]+.[\w]+$/,
+        "El correo es inválido, hola@hola.com"
         ],
+
+        //Expresión regular fecha
+        fecha: [
+            /^(\d{4})(\/|-)(\d{1,2})(\/|-)(\d{1,2})$/,
+            "El formato debe ser YYYY/MM/DD"
+        ],
+
+        //Expresión regular hora
         hora: [/\d\d[:]\d\d$/,
-            "La hora es errónea. Ejemplo: 11:40"
+        "La hora es errónea. Ejemplo: 11:40"
         ],
+
+        //Expresión regular noches
+        noches: [/[1-9]{1,}$/,
+            "El número de noches es inválido"
+        ],
+
+        //Expresión regular personas
+        personas: [/[1-9]{1,}$/,
+            "El número de personas es inválido"
+        ]
+   
     }
 
     //Objeto tester necesario para reserva.
     let reserva = {
         //Valores por defecto con expresiones
         test(patron, valor, msg) {
-            if (!patron[0].test(valor.value)) {
+            let regex = new RegExp(patron[0]);
+            if (!regex.test(valor.value)) {
                 msg.textContent = patron[1];
             } else
                 reserva.limpiarValores(msg, xSpan);
         },
         //Limpio los valores de la reserva.
-        limpiarValores(span, xSpan){
+        limpiarValores(span, xSpan) {
             span.textContent = "";
             xSpan.textContent = "";
         }
@@ -107,18 +108,18 @@
      * @param {*} input acción del valor blur.
      * Activa la validación en los eventos.
      */
-    function validarBlur(input){
-        inputs.forEach(function(valor, indice){
+    function validarBlur(input) {
+        inputs.forEach(function (valor, indice) {
             //Disparador.
-            if(input === "seleccionado"){ //Lo asocio más adelante en botonReservar
-                valor.addEventListener("blur", function(){
+            if (input === "seleccionado") { //Lo asocio más adelante en botonReservar
+                valor.addEventListener("blur", function () {
                     validarInputs(valor, indice);
                 }());
 
-            
-            } else{
-                valor.addEventListener("blur", function(){
-                   validarInputs(valor, indice); 
+
+            } else {
+                valor.addEventListener("blur", function () {
+                    validarInputs(valor, indice);
                 });
             }
         });
@@ -130,54 +131,13 @@
      * @param {*} valor obtengo la classe de los elementos del DOM
      * @param {*} span es el indice del elemento ( se muestra mensaje.)
      */
-    function validarInputs(valor, span){
+    function validarInputs(valor, span) {
         //Obtengo la clase de cada input
         if (valor.getAttribute("class")) {
             //Mi objeto tester
             reserva.test(patron[valor.getAttribute("class")], valor, spans[span]);
-          }
+        }
     }
-    // /**
-    //  * Valido el nombre
-    //  */
-    // function validarNombre() {
-    //     reserva.testValores(nombre, xNombre, "nombre");
-    // }
-
-    // /**
-    //  * Valido las noches
-    //  */
-    // function validarNoches() {
-    //     reserva.testValores(noches, xNoches, "noches");
-    // }
-
-    // /**
-    //  * Valido las personas
-    //  */
-    // function validarPersonas() {
-    //     reserva.testValores(personas, xPersonas, "personas");
-    // }
-
-    // /**
-    //  * Valido el correo
-    //  */
-    // function validarCorreo() {
-    //     reserva.test(patron.correo, correo, xCorreo, "correo");
-    // }
-
-    // /**
-    //  * Valido la hora
-    //  */
-    // function validarHora() {
-    //     reserva.test(patron.hora, hora, xHora, "hora");
-    // }
-
-    // /**
-    //  * Valido la fecha
-    //  */
-    // function validarFecha() {
-    //     reserva.testFecha(fecha, xFecha, "fecha");
-    // }
 
     /**
      * Valido la edad, dependiendo de los checkeds
@@ -205,42 +165,51 @@
     }
 
     /**
+     * Obtengo un array con los indices
+     * de los spans
+     */
+    function indiceErrones() {
+        let inputErroneos = [];
+        spans.forEach((valor, indice) => {
+            if (valor.textContent !== "") {
+                inputErroneos.push(indice)
+            }
+        });
+        return inputErroneos;
+    }
+
+    /**
      * Valido la reserva
      */
     function validarReserva() {
-        try {
-            validarNombre();
-            validarCorreo();
-            validarFecha();
-            validarHora();
-            validarNoches();
-            validarPersonas();
+        //Activo el disparador.
+        validarBlur("seleccionado");
 
-            if (map.size > 0) {
-                xSpan.textContent = "";
-                map.forEach(element => {
-                    element.focus();
-                    throw false;
-                });
-            } else if (map.size === 0) {
-                xSpan.textContent = "";
-                try {
-                    let reserva = new Reserva(
-                        nombre.value,
-                        correo.value,
-                        new Date(fecha.value),
-                        hora.value,
-                        noches.value,
-                        personas.value,
-                        validarEdad(),
-                        validarComida()
-                    );
-                    reserva.mostrar();
-                } catch (e) {
-                    xSpan.textContent = e.message;
-                }
-            }
-        } catch (e) {}
+        //Recorro el array y NO hago uso del map
+        if (indiceErrones().length > 0) {
+            inputs[indiceErrones()[0]].focus();
+            return;
+        }
+
+        //Vacio el span global.
+        //Muestra el error de fecha.
+        xSpan.textContent = "";
+
+        try {
+            let reserva = new Reserva(
+                inputs[0].value,
+                inputs[1].value,
+                new Date(inputs[2].value),
+                inputs[3].value,
+                inputs[4].value,
+                inputs[5].value,
+                validarEdad(),
+                validarComida()
+              );
+              reserva.mostrar();//Mustro los datos
+        } catch (e) {
+            xSpan.textContent = e.message;
+        }
     }
     window.addEventListener('load', init);
 }
