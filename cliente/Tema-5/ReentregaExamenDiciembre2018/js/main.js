7a2e5f6f2252ad79e872ef6619dec84786286165
Francisco Ramirez <ramiirez100@gmail.com>
2019-01-27 11:57:37 +0100

    Recorrer collection input "blur" Closes #23

--- cliente/Tema-5/ReentregaExamenDiciembre2018/js/main.js
+++ cliente/Tema-5/ReentregaExamenDiciembre2018/js/main.js
@@ -42,12 +42,6 @@
     function init() {
 
         //Obtengo id de los inputs
-        // nombre = document.getElementById('nombre');
-        // correo = document.getElementById('correo');
-        // fecha = document.getElementById('fecha');
-        // hora = document.getElementById('hora');
-        // noches = document.getElementById('noches');
-        // personas = document.getElementById('personas');
         inputs = Array.from(document.querySelectorAll("input:not([type='button']):not([type='checkbox']):not([type='radio'])"));
 
         //Id de los checks
@@ -59,24 +53,19 @@
         cena = document.getElementById('cena');
 
         //Id de los spans
-        // xNombre = document.getElementById('xNombre');
-        // xCorreo = document.getElementById('xCorreo');
-        // xFecha = document.getElementById('xFecha');
-        // xHora = document.getElementById('xHora');
-        // xNoches = document.getElementById('xNoches');
-        // xPersonas = document.getElementById('xPersonas');
-        // xSpan = document.getElementById('xSpan');
+        spans = Array.from(document.querySelectorAll("body form span"));
 
         //Id de boton enviar
         enviar = document.getElementById('enviar');
 
         //Eventos de Blur en las cajas.
-        nombre.addEventListener('blur', validarNombre);
-        correo.addEventListener('blur', validarCorreo);
-        fecha.addEventListener('blur', validarFecha);
-        hora.addEventListener('blur', validarHora);
-        noches.addEventListener('blur', validarNoches);
-        personas.addEventListener('blur', validarPersonas);
+        // nombre.addEventListener('blur', validarNombre);
+        // correo.addEventListener('blur', validarCorreo);
+        // fecha.addEventListener('blur', validarFecha);
+        // hora.addEventListener('blur', validarHora);
+        // noches.addEventListener('blur', validarNoches);
+        // personas.addEventListener('blur', validarPersonas);
+        validarBlur(); //Válido los inputs con blur de la colecction inputs.
 
         //Evento click del botón
         enviar.addEventListener("click", validarReserva);
@@ -128,46 +117,82 @@
     };
 
     /**
-     * Valido el nombre
+     * Recorro mi collection de inputs, dependiendo del tipo se asocia.
+     * 
+     * @param {*} input acción del valor blur.
+     * Activa la validación en los eventos.
      */
-    function validarNombre() {
-        reserva.testValores(nombre, xNombre, "nombre");
+    function validarBlur(input){
+        inputs.forEach(function(valor, indice){
+            //Disparador.
+            if(input === "seleccionado"){ //Lo asocio más adelante en botonReservar
+                valor.addEventListener("blur", function(){
+                    validarInputs(valor, indice);
+                }());
+
+            
+            } else{
+                valor.addEventListener("blur", function(){
+                   validarInputs(valor, indice); 
+                });
+            }
+        });
     }
 
     /**
-     * Valido las noches
+     * Se encarga de validar los inputs.
+     * 
+     * @param {*} valor obtengo la classe de los elementos del DOM
+     * @param {*} span es el indice del elemento ( se muestra mensaje.)
      */
-    function validarNoches() {
-        reserva.testValores(noches, xNoches, "noches");
+    function validarInputs(valor, span){
+        //Obtengo la clase de cada input
+        if (valor.getAttribute("class")) {
+            //Mi objeto tester
+            reserva.test(patrones[valor.getAttribute("class")], valor, spans[span]);
+          }
     }
+    // /**
+    //  * Valido el nombre
+    //  */
+    // function validarNombre() {
+    //     reserva.testValores(nombre, xNombre, "nombre");
+    // }
 
-    /**
-     * Valido las personas
-     */
-    function validarPersonas() {
-        reserva.testValores(personas, xPersonas, "personas");
-    }
+    // /**
+    //  * Valido las noches
+    //  */
+    // function validarNoches() {
+    //     reserva.testValores(noches, xNoches, "noches");
+    // }
 
-    /**
-     * Valido el correo
-     */
-    function validarCorreo() {
-        reserva.test(patron.correo, correo, xCorreo, "correo");
-    }
+    // /**
+    //  * Valido las personas
+    //  */
+    // function validarPersonas() {
+    //     reserva.testValores(personas, xPersonas, "personas");
+    // }
 
-    /**
-     * Valido la hora
-     */
-    function validarHora() {
-        reserva.test(patron.hora, hora, xHora, "hora");
-    }
+    // /**
+    //  * Valido el correo
+    //  */
+    // function validarCorreo() {
+    //     reserva.test(patron.correo, correo, xCorreo, "correo");
+    // }
 
-    /**
-     * Valido la fecha
-     */
-    function validarFecha() {
-        reserva.testFecha(fecha, xFecha, "fecha");
-    }
+    // /**
+    //  * Valido la hora
+    //  */
+    // function validarHora() {
+    //     reserva.test(patron.hora, hora, xHora, "hora");
+    // }
+
+    // /**
+    //  * Valido la fecha
+    //  */
+    // function validarFecha() {
+    //     reserva.testFecha(fecha, xFecha, "fecha");
+    // }
 
     /**
      * Valido la edad, dependiendo de los checkeds
