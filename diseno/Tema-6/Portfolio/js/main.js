e62091b47a8944de59169b9c68bc5e8467de6c41
Francisco Ramirez <ramiirez100@gmail.com>
2019-01-29 22:46:58 +0100

    Portfolio mejoras wheel

--- diseno/Tema-6/Portfolio/js/main.js
+++ diseno/Tema-6/Portfolio/js/main.js
@@ -10,51 +10,34 @@
  * Funcion inicial del programa
  */
 function init() {
-    let $section = ($('section').length);
-    $('section').on('mousewheel touchmove', function (event) {
 
-        let sectionAtm;
-        let idAtm = event.currentTarget.id;
+    if ($(window).width() >= 425) {  
 
-        //WheelDelta devuelve siempre múltiplos de 120.
-        if (event.originalEvent.wheelDelta / 120 > 0) {
-            if (parseInt(idAtm) - 1 != 0) {
-                sectionAtm = "#" + (parseInt(idAtm) - 1);
-                if ($(sectionAtm) != undefined)
-                    moverSection(sectionAtm);
+        let $section = ($('section').length);
+        $('section').on('mousewheel', function (event) {
+    
+            let sectionAtm;
+            let idAtm = event.currentTarget.id;
+    
+            //WheelDelta devuelve siempre múltiplos de 120.
+            if (event.originalEvent.wheelDelta / 120 > 0) {
+                if (parseInt(idAtm) - 1 != 0) {
+                    sectionAtm = "#" + (parseInt(idAtm) - 1);
+                    if ($(sectionAtm) != undefined)
+                        moverSection(sectionAtm);
+                }
+    
+            } else {
+                if (parseInt(idAtm) != $section) {
+                    sectionAtm = "#" + (parseInt(idAtm) + 1);
+                    if ($(sectionAtm) != undefined)
+                        moverSection(sectionAtm);
+                }
             }
+            console.log("prueba");
+        });
 
-        } else {
-            if (parseInt(idAtm) != $section) {
-                sectionAtm = "#" + (parseInt(idAtm) + 1);
-                if ($(sectionAtm) != undefined)
-                    moverSection(sectionAtm);
-            }
-        }
-        console.log("prueba");
-    });
-
-    let collected = [];
-
-    $('section').on('touchstart touchmove touchend', function (event) {
-        event.preventDefault();
-        if (event.type == 'touchstart') {
-            collected = [];
-        } else if (event.type == 'touchmove') {
-            if (parseInt(idAtm) - 1 != 0) {
-                sectionAtm = "#" + (parseInt(idAtm) - 1);
-                if ($(sectionAtm) != undefined)
-                    moverSection(sectionAtm);
-            }
-        } else if (event.type == 'touchend') {
-            if (parseInt(idAtm) != $section) {
-                sectionAtm = "#" + (parseInt(idAtm) + 1);
-                if ($(sectionAtm) != undefined)
-                    moverSection(sectionAtm);
-            }
-        }
-    });
-
+ }   
 
 }
 
