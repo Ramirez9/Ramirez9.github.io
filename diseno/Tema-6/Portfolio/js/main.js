19eeecfbfd99dab53989859ad7d4c80f55b06c09
Francisco Ramirez <ramiirez100@gmail.com>
2019-01-30 19:47:11 +0100

    Mejoras portfolio

--- diseno/Tema-6/Portfolio/js/main.js
+++ diseno/Tema-6/Portfolio/js/main.js
@@ -10,37 +10,65 @@
  * Funcion inicial del programa
  */
 function init() {
+    //Anulo el botón inicial de top
+    $(".subir").css("display", "none");
+    let $section = ($('section').length);
+    $('section').on('mousewheel', function (event) {
 
-    if ($(window).width() >= 425) {  
+        let sectionAtm;
+        let idAtm = event.currentTarget.id;
 
-        let $section = ($('section').length);
-        $('section').on('mousewheel', function (event) {
-    
-            let sectionAtm;
-            let idAtm = event.currentTarget.id;
-    
-            //WheelDelta devuelve siempre múltiplos de 120.
-            if (event.originalEvent.wheelDelta / 120 > 0) {
-                if (parseInt(idAtm) - 1 != 0) {
-                    sectionAtm = "#" + (parseInt(idAtm) - 1);
-                    if ($(sectionAtm) != undefined)
-                        moverSection(sectionAtm);
-                }
-    
-            } else {
-                if (parseInt(idAtm) != $section) {
-                    sectionAtm = "#" + (parseInt(idAtm) + 1);
-                    if ($(sectionAtm) != undefined)
-                        moverSection(sectionAtm);
-                }
+        //WheelDelta devuelve siempre múltiplos de 120.
+        if (event.originalEvent.wheelDelta / 120 > 0) {
+            if (parseInt(idAtm) - 1 != 0) {
+                sectionAtm = "#" + (parseInt(idAtm) - 1);
+                if ($(sectionAtm) != undefined)
+                    moverSection(sectionAtm);
             }
-            console.log("prueba");
-        });
 
- }   
+        } else {
+            if (parseInt(idAtm) != $section) {
+                sectionAtm = "#" + (parseInt(idAtm) + 1);
+                if ($(sectionAtm) != undefined)
+                    moverSection(sectionAtm);
+            }
+        }
+        console.log("prueba");
+    });
 
-}
 
+    let contador = 1;
+
+    $(".subir, .bajar").on("click", function (event) {
+
+        let sectionAtm;
+
+        event.preventDefault();
+        if ($(this)[0].id == "subir") {
+            sectionAtm = "#" + (--contador);
+            if ($(sectionAtm) != undefined)
+                moverSection(sectionAtm);
+
+        } else {
+            sectionAtm = "#" + (++contador);
+            if ($(sectionAtm) != undefined)
+                moverSection(sectionAtm);
+        }
+
+        if (contador > 1)
+            $(".subir").css("display", "block");
+        else
+            $(".subir").css("display", "none");
+
+        if (contador != 4)
+            $(".bajar").css("display", "block");
+        else
+            $(".bajar").css("display", "none");
+
+    });
+
+    setTimeout(barras, 1000);
+}  
 /**
  * Obtiene un parametro al cual le aplica un efecto de 1000 segundos en desplazarse
  */
@@ -51,4 +79,15 @@ function moverSection(msg) {
 
 
 }
+
+function barras(){
+    $(".barra-habilidad").each(function () {
+        let barra = parseInt($(this).html());
+            $(this).animate({
+                'width': '' + barra + '%'
+            }, 10000);   
+    });
+}
+
+
 $(init);
