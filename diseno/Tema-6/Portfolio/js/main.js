dd6c3bfa1ae20918d656156e735141ce953c5fde
Francisco Ramirez <ramiirez100@gmail.com>
2019-01-31 21:35:14 +0100

    Portfolio 1.1

--- diseno/Tema-6/Portfolio/js/main.js
+++ diseno/Tema-6/Portfolio/js/main.js
@@ -12,12 +12,16 @@
 function init() {
     //Anulo el botón inicial de top
     $(".subir").css("display", "none");
-    let $section = ($('section').length);
-    $('section').on('mousewheel', function (event) {
+   // if ($(window).width() >= 1020) {  
+    let $section = ($('section, footer').length);
+    $('#1, #2, #3, #4, #5').on('mousewheel', function (event) {
 
         let sectionAtm;
         let idAtm = event.currentTarget.id;
-
+        $(".foto-perfil").css({
+            "transition-duration" : "5s",
+            "transform": "rotateY(360deg)"
+        })
         //WheelDelta devuelve siempre múltiplos de 120.
         if (event.originalEvent.wheelDelta / 120 > 0) {
             if (parseInt(idAtm) - 1 != 0) {
@@ -36,7 +40,7 @@ function init() {
         console.log("prueba");
     });
 
-
+   // }
     let contador = 1;
 
     $(".subir, .bajar").on("click", function (event) {
@@ -60,7 +64,7 @@ function init() {
         else
             $(".subir").css("display", "none");
 
-        if (contador != 4)
+        if (contador != 5)
             $(".bajar").css("display", "block");
         else
             $(".bajar").css("display", "none");
@@ -68,6 +72,15 @@ function init() {
     });
 
     setTimeout(barras, 1000);
+
+	$("#idiomas").change(function(){
+        if($("#idiomas").val()==="français")
+        $(".icono-texto").text("franchute")
+        if($("#idiomas").val()==="english")
+        $(".icono-texto").text("ingles")
+        if($("#idiomas").val()==="español")
+        alert("español");
+	});
 }  
 /**
  * Obtiene un parametro al cual le aplica un efecto de 1000 segundos en desplazarse
@@ -82,9 +95,9 @@ function moverSection(msg) {
 
 function barras(){
     $(".barra-habilidad").each(function () {
-        let barra = parseInt($(this).html());
+        let valorTexto = parseInt($(this).html());
             $(this).animate({
-                'width': '' + barra + '%'
+                'width': '' + valorTexto + '%'
             }, 10000);   
     });
 }
